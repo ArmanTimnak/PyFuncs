81290bb02b124de18f56272c66afd1d8eda1f83c
Arman Timnak <63553676+ArmanTimnak@users.noreply.github.com>
2023-02-09 18:07:34 +0330

    Update fib_seq.py

--- math/fib_seq.py
+++ math/fib_seq.py
@@ -1,7 +1,7 @@
 # The fibonacci sequence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers.
 # The first two numbers in the sequence are 0 and 1.
 
-def fib_seq(n): # The function fib_seq takes in a number n and returns the n'th number in the fibonacci sequence.
+def fib_seq(n: int) -> int: # The function fib_seq takes in a number n and returns the n'th number in the fibonacci sequence.
     if n<0: 
         print("Incorrect input") 
     elif n==1: # When n is equal to 1, the function returns 0.
